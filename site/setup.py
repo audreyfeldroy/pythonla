504abb0b7b3bae3f074e68ff20e909277680dce9
Michael R <lapython-gh@webhippo.net>
2011-05-29 16:01:09 -0700

    add pyramid_beaker and pyramid_jinja2 to setup.py requirements

--- site/setup.py
+++ site/setup.py
@@ -9,6 +9,8 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
+    'pyramid_jinja2',
+    'pyramid_beaker',
     'SQLAlchemy',
     'transaction',
     'repoze.tm2>=1.0b1', # default_commit_veto
