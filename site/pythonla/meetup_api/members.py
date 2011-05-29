0025aed5d0f580f16c6e3dc97deb1613eab07e5b
pydanny <pydanny@gmail.com>
2011-05-29 16:17:29 -0700

    Adding the fundamental bits of the meetup_api module

---
+++ site/pythonla/meetup_api/members.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+from .settings import API_KEY, GROUP_NAME
+
+base_url = "https://api.meetup.com/members.json"
+
+group_name = "lapython"
+get_data = dict(
+    group_urlname=GROUP_NAME,
+    key=API_KEY
+    )
+
+def get_members():
+    
+    r = requests.get(base_url, get_data)
+    if r.status_code == 200:
+        data = json.loads(r.content)
+    else:
+        raise Exception()
+    
+    for member in data['results']:
+        print member['lat'], member['lon']
+        
+get_members()
---
+++ site/pythonla/meetup_api/settings.py
@@ -0,0 +1,2 @@
+API_KEY = "5f29565e357337e6b607b71302b96d"
+GROUP_NAME = "lapython"
