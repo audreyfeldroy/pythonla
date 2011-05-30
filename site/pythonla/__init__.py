f60f928b49bfa841f54c7722cdc5fb92c7368c84
Michael R <lapython-gh@webhippo.net>
2011-05-29 21:28:42 -0700

    add basic todo and done files

--- site/pythonla/__init__.py
+++ site/pythonla/__init__.py
@@ -1,5 +1,5 @@
-from pyramid_beaker import (session_factory_from_settings, 
-                            set_cache_regions_from_settings)
+from pyramid_beaker import session_factory_from_settings
+from pyramid_beaker import set_cache_regions_from_settings
 
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
@@ -10,16 +10,30 @@ def main(global_config, **settings):
     """
     This function returns a Pyramid WSGI application.
     """
+
+    # SQLAlchemy
     engine = engine_from_config(settings, 'sqlalchemy.')
     initialize_sql(engine)
+
+    # begin beaker setup
     session_factory = session_factory_from_settings(settings)
     set_cache_regions_from_settings(settings)
+
+    # basic config
     config = Configurator(settings=settings)
+
+    # static url setup
     config.add_static_view('static', 'pythonla:static')
+
+    # 
     config.include('pyramid_jinja2')
+
     config.begin()
     config.set_session_factory(session_factory)
     config.end()
+
+
+
     config.include(basic_routes)
     config.scan()
     return config.make_wsgi_app()
