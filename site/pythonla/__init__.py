afd45e71d327c687368ad0fa95f576a4fffdfb34
pyeek <me@pyeek.com>
2011-05-29 17:36:05 -0700

    Added beaker configuration

--- site/pythonla/__init__.py
+++ site/pythonla/__init__.py
@@ -1,3 +1,6 @@
+from pyramid_beaker import (session_factory_from_settings, 
+                            set_cache_regions_from_settings)
+
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
@@ -8,10 +11,15 @@ def main(global_config, **settings):
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
     initialize_sql(engine)
+    session_factory = session_factory_from_settings(settings)
+    set_cache_regions_from_settings(settings)
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_route('home', '/', view='pythonla.views.my_view',
                                  view_renderer='templates/mytemplate.jinja2')
+    config.begin()
+    config.set_session_factory(session_factory)
+    config.end()
     return config.make_wsgi_app()
 
 
