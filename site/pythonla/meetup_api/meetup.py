4ff354cde341a9e6cbaaa5075d4728eb1b5e226a
jathanism <jathan@gmail.com>
2011-05-29 17:24:01 -0700

    move events code into meetup.py

--- site/pythonla/meetup_api/meetup.py
+++ site/pythonla/meetup_api/meetup.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import json
 import requests
 
@@ -9,13 +11,22 @@ REQUEST_DATA = dict(
     key=settings.API_KEY,
 )
 
+# Exports
+__all__ = ('Meetup', 'NotJSONContent',)
+
+
+# Exceptions
 class NotJSONContent(Exception):
     pass
 
+
+# Classes
 class Meetup(object):
     
     def make_request(self, url, request_data):
-        
+        """
+        Boilerplate request fetcher that returns the "results" attribute from the API
+        """
         r = requests.get(url, request_data)
         if r.status_code == 200:
             try:
@@ -28,26 +39,33 @@ class Meetup(object):
             # TODO pass in proper message
             raise Exception()
         return obj.results        
-        
 
     def get_members(self):
-        '''
+        """
         Get a blob of member data and return a recurisve DictObject of the
         same.
-        '''
+        """
         url = settings.BASE_URL + 'members.json'
         
         return self.make_request(url, request_data=REQUEST_DATA)
     
     def get_profiles(self):
-        '''
+        """
         Get a blob of profiles data and return a recurisve DictObject of the
         same.
-        '''
+        """
         url = settings.BASE_URL + '2/profiles.json'
             
         return self.make_request(url, request_data=REQUEST_DATA)            
-    
+
+    def get_events():
+        """
+        Get a blob of events data and return a recurisve DictObject of the same.
+        """
+        url = settings.BASE_URL + 'events.json'
+
+        return self.make_request(url, request_data=REQUEST_DATA)
+
 
 if __name__ == "__main__":
     m = Meetup()
