7e3320150fc17990a445c0d905958a7ca983d63a
pydanny <pydanny@gmail.com>
2011-05-29 17:17:40 -0700

    Moving members to be meetup

--- site/pythonla/meetup_api/meetup.py
+++ site/pythonla/meetup_api/meetup.py
@@ -4,26 +4,52 @@ import requests
 import utils
 import settings
 
-url = settings.BASE_URL + 'members.json'
-
-get_data = dict(
+REQUEST_DATA = dict(
     group_urlname=settings.GROUP_NAME,
     key=settings.API_KEY,
 )
 
-def get_members():
-    '''
-    Get a blob of member data and return a recurisve DictObject of the
-    same.
-    '''
-    r = requests.get(url, get_data)
-    if r.status_code == 200:
-        data = json.loads(r.content)
-        obj = utils.recursive_dictobject(data)
-    else:
-        raise Exception()
-    
-    for member in obj.results:
-        print member.name + ":", member.lat, member.lon
+class NotJSONContent(Exception):
+    pass
 
-    return obj
+class Meetup(object):
+    
+    def make_request(self, url, request_data):
+        
+        r = requests.get(url, request_data)
+        if r.status_code == 200:
+            try:
+                data = json.loads(r.content)
+            except ValueError:
+                raise NotJSONContent(r.content)
+            obj = utils.recursive_dictobject(data)
+        else:
+            # TODO change to custom exception
+            # TODO pass in proper message
+            raise Exception()
+        return obj.results        
+        
+
+    def get_members(self):
+        '''
+        Get a blob of member data and return a recurisve DictObject of the
+        same.
+        '''
+        url = settings.BASE_URL + 'members.json'
+        
+        return self.make_request(url, request_data=REQUEST_DATA)
+    
+    def get_profiles(self):
+        '''
+        Get a blob of profiles data and return a recurisve DictObject of the
+        same.
+        '''
+        url = settings.BASE_URL + '2/profiles.json'
+            
+        return self.make_request(url, request_data=REQUEST_DATA)            
+    
+
+if __name__ == "__main__":
+    m = Meetup()
+    print m.get_members()[0].bio
+    print len(m.get_profiles())
