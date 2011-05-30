034ca11c325d0b21c956368b779982f7e21f1e58
jathanism <jathan@gmail.com>
2011-05-29 17:50:45 -0700

    add members view

--- site/pythonla/views.py
+++ site/pythonla/views.py
@@ -2,7 +2,14 @@ from pythonla.models import DBSession
 from pythonla.models import MyModel
 from pyramid.view import view_config
 
+from meetup_api.meetup import Meetup
+meetup = Meetup()
 
 @view_config(renderer='templates/mytemplate.jinja2')
 def my_view(request):
-        return {'foo': 1, 'bar': 2}
+    return {'foo': 1, 'bar': 2}
+
+@view_config(renderer='templates/members.jinja2')
+def members_view(request):
+    members = meetup.get_members()
+    return {'members': members}
